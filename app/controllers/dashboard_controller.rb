4ea370ee094307f31fd54fc476bbb8ba693f2179
Recker Swartz <reckerswartz@hotmail.com>
2022-08-11 05:42:08 +0530

    add rotation form

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,21 +1,37 @@
+# frozen_string_literal: true
+
 class DashboardController < ApplicationController
   def index
-    @connection_status = "Not connected to database server"
+    @connection_status = 'Not connected to database server'
   end
 
   def connect_to_database
-    # this takes a hash of options, almost all of which map directly
-    # to the familiar database.yml in rails
-    # See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/Mysql2Adapter.html
-    @connection = Mysql2::Client.new(:host => database_params[:host], :username => database_params[:username], :password => database_params[:password], :database => database_params[:database])
+    @connection_status = 'Could not connect to database server'
+    begin
+      # get the secret username and password for the database from vault
+      vault_secret = []
+      Vault.with_retries(Vault::HTTPConnectionError, Vault::HTTPError, attempts: 5) do
+        vault_secret = Vault.logical.read(database_params[:secret_path])
+      end
+      @username = vault_secret.data[:username]
+      @password = vault_secret.data[:password]
 
-    # check connection to database server by pinging it
-    # if the ping fails, the connection will be nil
-    # if the ping succeeds, the connection will be a Mysql2::Client object
-    if @connection.ping
-      @connection_status = "Connected to database server"
-    else
-      @connection_status = "Could not connect to database server"
+      # this takes a hash of options, almost all of which map directly
+      # to the familiar database.yml in rails
+      # See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/Mysql2Adapter.html
+      @connection = Mysql2::Client.new(host: database_params[:host], username: @username, password: @password,
+                                       database: database_params[:database])
+
+      # check connection to database server by pinging it
+      # if the ping fails, the connection will be nil
+      # if the ping succeeds, the connection will be a Mysql2::Client object
+      @connection_status = if @connection.ping
+                             'Connected to database server'
+                           else
+                             'Could not connect to database server'
+                           end
+    rescue StandardError => e
+      @connection_status = 'Could not connect to database server'
     end
 
     ## respond format is TURBO_STREAM
@@ -25,31 +41,25 @@ class DashboardController < ApplicationController
   end
 
   def rotate_vault_secret
-    # rotate the secret in vault
-    Vault.with_retries(Vault::HTTPConnectionError, Vault::HTTPError, attempts: 5) do
-      Vault.logical.write("<path entered in text box>", { :value => "<new secret>" })
+    @rotate_status = 'Could not rotate secret'
+    begin
+      # rotate the secret in vault
+      Vault.with_retries(Vault::HTTPConnectionError, Vault::HTTPError, attempts: 5) do
+        Vault.logical.write(params[:rotate_secrets][:rotate_path])
+      end
+      @rotate_status = 'Secret rotated'
+    rescue StandardError => e
+      @rotate_status = e.message
     end
 
     respond_to do |format|
-      format.turbo_stream do
-        render plain: "Vault secret rotated"
-      end
+      format.turbo_stream
     end
-
   end
 
   private
 
   def database_params
-    params.require(:database).permit(:host, :username, :password, :database)
-  end
-
-  def get_vault_secret
-    ## we would not be getting a static secret,
-    ## but rather calling "vault read" to generate the credentials
-    # vault_secret = Vault::Secret.find("secret/database")
-    Vault.with_retries(Vault::HTTPConnectionError, Vault::HTTPError, attempts: 5) do
-      secret = Vault.logical.read("<path entered in text box>")
-    end
+    params.require(:database).permit(:host, :database, :secret_path)
   end
 end
